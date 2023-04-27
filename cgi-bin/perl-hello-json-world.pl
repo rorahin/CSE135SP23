08b21c8616cd049008d0278c366517a4ee922330
greyluo <g1luo@ucsd.edu>
2023-04-27 23:19:28 +0000

    debub

--- cgi-bin/perl-hello-json-world.pl
+++ cgi-bin/perl-hello-json-world.pl
@@ -1,1 +1,14 @@
-{"heading":"Hello, Perl!","IP":"69.196.46.34","title":"Hello, Perl!","time":"Thu Apr 27 21:47:20 2023","message":"This response was generated with the Perl programming language"}
+#!/usr/bin/perl
+# In Perl, you must first install the JSON package from CPAN (the Perl equivalent to npm)
+use JSON;
+
+print "Cache-Control: no-cache\n";
+print "Content-type: application/json\n\n";
+
+$date = localtime();
+$address = $ENV{REMOTE_ADDR};
+
+my %message = ('title' => 'Hello, Perl!', 'heading' => 'Hello, Perl!', 'message' => 'This page was generated with the Perl programming language', 'time' => $date, 'IP' => $address);
+
+my $json = encode_json \%message;
+print "$json\n";
