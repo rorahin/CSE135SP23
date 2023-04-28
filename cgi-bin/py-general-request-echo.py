72ec32866a33d3261b89a77ede844f3d8082c4fb
greyluo <luo80480@gmail.com>
2023-04-27 17:50:37 -0700

    fix

--- cgi-bin/py-general-request-echo.py
+++ cgi-bin/py-general-request-echo.py
@@ -1,25 +1,37 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+
 import os
 import sys
-import urllib.parse
 
-print("Content-Type: text/plain")
+# Set the content type and disable caching
+print("Content-type: text/html")
+print("Cache-Control: no-cache")
 print()
 
-request_method = os.environ.get("REQUEST_METHOD", "GET")
+# Print the HTML file top
+print("""<!DOCTYPE html>
+<html>
+<head>
+    <title>General Request Echo</title>
+</head>
+<body>
+    <h1 align="center">General Request Echo</h1>
+    <hr>
+""")
+
+# Print HTTP Protocol, HTTP Method, and the Query String
+print("<p><b>HTTP Protocol:</b>", os.environ["SERVER_PROTOCOL"], "</p>")
+print("<p><b>HTTP Method:</b>", os.environ["REQUEST_METHOD"], "</p>")
+print("<p><b>Query String:</b>", os.environ["QUERY_STRING"], "</p>")
+
+# Get the form data from the standard input
+form_data = ""
 content_length = int(os.environ.get("CONTENT_LENGTH", 0))
+if content_length > 0:
+    form_data = sys.stdin.read(content_length)
 
-print(f"Request method: {request_method}")
+# Print the message body
+print("<p><b>Message Body:</b>", form_data, "</p>")
 
-if request_method == "POST":
-    post_data = sys.stdin.read(content_length)
-    params = urllib.parse.parse_qs(post_data)
-elif request_method == "GET":
-    query_string = os.environ.get("QUERY_STRING", "")
-    params = urllib.parse.parse_qs(query_string)
-else:
-    params = {}
-
-for key, values in params.items():
-    for value in values:
-        print(f"{key}: {value}")
+# Print the HTML file bottom
+print("</body></html>")
