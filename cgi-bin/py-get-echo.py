959a7ee829bbbb0474645dbbc0a6912192a7104b
greyluo <luo80480@gmail.com>
2023-04-27 17:58:56 -0700

    fix

--- cgi-bin/py-get-echo.py
+++ cgi-bin/py-get-echo.py
@@ -1,36 +1,19 @@
-#!/usr/bin/env python3
-
 import os
-import urllib.parse
 
-# Set the content type and disable caching
-print("Content-type: text/html")
+# Print HTML header
 print("Cache-Control: no-cache")
-print()
+print("Content-type: text/html\n")
+print("<html><head><title>GET query string</title></head>\
+    <body><h1 align=center>GET query string</h1>\
+    <hr/>\n")
 
-# Print the HTML file top
-print("""<!DOCTYPE html>
-<html>
-<head>
-    <title>GET Request Echo</title>
-</head>
-<body>
-    <h1 align="center">Get Request Echo</h1>
-    <hr>
-""")
+# Get and format query string
+query_string = os.getenv("QUERY_STRING")
+print("Raw query string: " + query_string + "<br/><br/>")
+print("<table> Formatted Query String:")
+query = query_string.split('&')
+for p in query:
+    var, val = p.split('=', 1) if '=' in p else (p, "<empty field>")
+    print("<tr><td>%-8s:</td><td>%s</td></tr>\n" % (var, val))
+print("</table>")
 
-# Get the query string from the environment variable
-query_string = os.environ.get("QUERY_STRING", "")
-
-# Parse the query string
-parsed_query_string = urllib.parse.parse_qs(query_string)
-
-# Print out the query string
-loop = 0
-for key, value in parsed_query_string.items():
-    loop += 1
-    if loop % 2 != 0:
-        print(key, "=", value[0], "<br/>")
-
-# Print the HTML file bottom
-print("</body></html>")
--- cgi-bin/py-post-echo.py
+++ cgi-bin/py-post-echo.py
@@ -1,41 +1,16 @@
-#!/usr/bin/env python3
+import sys
 
-import os
-import urllib.parse
-
-# Set the content type and disable caching
-print("Content-type: text/html")
+# Print HTML header
 print("Cache-Control: no-cache")
-print()
+print("Content-type: text/html\n")
+print("<html><head><title>POST Message Body</title></head>\
+    <body><h1 align=center>POST Message Body</h1>\
+    <hr/>\n")
 
-# Print the HTML file top
-print("""<!DOCTYPE html>
-<html>
-<head>
-    <title>POST Request Echo</title>
-</head>
-<body>
-    <h1 align="center">POST Request Echo</h1>
-    <hr>
-""")
+# Get message body from stdin
+message_body = sys.stdin.read()
+print("Message Body: " + message_body + "<br/>")
 
-# Get the form data from the standard input
-form_data = ""
-content_length = int(os.environ.get("CONTENT_LENGTH", 0))
-if content_length > 0:
-    form_data = sys.stdin.read(content_length)
-
-# Parse the form data
-parsed_form_data = urllib.parse.parse_qs(form_data)
-
-# Print out the form data
-print("<b>Message Body:</b><br/>")
-print("<ul>")
-loop = 0
-for key, value in parsed_form_data.items():
-    loop += 1
-    if loop % 2 != 0:
-        print("<li>", key, "=", value[0], "</li>")
-print("</ul>")
-
-# Print the HTML file bottom
+# Print HTML footer
+print("</body>")
+print("</html>")
