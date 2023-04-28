92fb2ae72ed70be4597a2ae14658358ed23b1d61
greyluo <luo80480@gmail.com>
2023-04-27 17:26:22 -0700

    fix

--- cgi-bin/py-env.py
+++ cgi-bin/py-env.py
@@ -6,4 +6,4 @@ print("Content-Type: text/html")
 print()
 print("<html><head><title>Python Environment Variables</title></head><body>")
 for key, value in os.environ.items():
-    print(f"{key}: {value}")
+    print(f"<p><b>{key}</b>: {value}</p>")
