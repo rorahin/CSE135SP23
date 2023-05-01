4cfee3bbc37f2f2ff43376f389b5d6d15396a8a1
rudyorahin <rudydinha@gmail.com>
2023-04-30 17:28:22 -0700

    php cockies

--- cgi-bin/php-destroy-session.php
+++ cgi-bin/php-destroy-session.php
@@ -1,16 +1,19 @@
 <?php
+// Start or resume a session
 session_start();
+
+// Unset all session variables
+$_SESSION = array();
+
+// Delete the session cookie
+if (isset($_COOKIE[session_name()])) {
+    setcookie(session_name(), '', time() - 42000, '/');
+}
+
+// Destroy the session
 session_destroy();
-setcookie("PHPSESSID", "", time() - 3600, "/");
+
+// Redirect to another page
+header('Location: /perl-cgiform.html');
+exit;
 ?>
-<html>
-<head>
-<title>PHP Session Destroyed</title>
-</head>
-<body>
-<h1>Session Destroyed</h1>
-<a href="/php-cgiform.html">Back to the PHP CGI Form</a><br />
-<a href="/php-sessions-1.php">Back to Page 1</a><br />
-<a href="/php-sessions-2.php">Back to Page 2</a>
-</body>
-</html>
