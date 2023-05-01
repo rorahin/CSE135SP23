8a0d0199c9501f9b011ac47266e1d1e45dffdd39
rudyorahin <rudydinha@gmail.com>
2023-04-30 17:10:24 -0700

    session php

--- cgi-bin/php-sessions-1.php
+++ cgi-bin/php-sessions-1.php
@@ -1,38 +1,24 @@
 <?php
-// Create a new PHP Session
 session_start();
 
-// Create CGI Object
-$cgi = new CGI();
-
-// Create a new Cookie from the Session ID
-$cookie = $cgi->cookie("CGISESSID", session_id());
-echo $cgi->header(array("cookie"=>$cookie));
-
-// Store Data in that PHP Session
-$name = isset($_SESSION['username']) ? $_SESSION['username'] : $cgi->param('username');
-$_SESSION["username"] = $name;
-
-echo "<html>";
-echo "<head>";
-echo "<title>PHP Sessions</title>";
-echo "</head>";
-echo "<body>";
-
-echo "<h1>PHP Sessions Page 1</h1>";
-
-if ($name){
-  echo "<p><b>Name:</b> $name</p>";
-}else{
-  echo "<p><b>Name:</b> You do not have a name set</p>";
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['username'])) {
+    $_SESSION['username'] = $_POST['username'];
+    header('Location: state-demo-2.php');
+    exit;
 }
-echo "<br/><br/>";
-echo "<a href=\"/cgi-bin/php-sessions-2.php\">Session Page 2</a><br/>";
-echo "<a href=\"/php-cgiform.html\">PHP CGI Form</a><br />";
-echo "<form style=\"margin-top:30px\" action=\"/cgi-bin/php-destroy-session.php\" method=\"get\">";
-echo "<button type=\"submit\">Destroy Session</button>";
-echo "</form>";
-
-echo "</body>";
-echo "</html>";
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>State Demo - Page 1</title>
+</head>
+<body>
+	<h1>State Demo - Page 1</h1>
+	<form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+		<label for="username">Enter your name:</label>
+		<input type="text" id="username" name="username">
+		<button type="submit">Submit</button>
+	</form>
+</body>
+</html>
--- cgi-bin/php-sessions-2.php
+++ cgi-bin/php-sessions-2.php
@@ -1,39 +1,23 @@
 <?php
-// Access PHP Session
 session_start();
 
-// Create a new CGI Object
-$cgi = new CGI();
-
-// Get the Session ID from the Cookie
-$sid = isset($_COOKIE['CGISESSID']) ? $_COOKIE['CGISESSID'] : null;
-$session = new CGI::Session(undef, $sid, {Directory=>'/tmp'});
-
-// Access Stored Data
-$name = $session->param("username");
-
-header("Content-Type: text/html");
-
-echo "<html>";
-echo "<head>";
-echo "<title>PHP Sessions</title>";
-echo "</head>";
-echo "<body>";
-
-echo "<h1>PHP Sessions Page 2</h1>";
-
-if ($name){
-	echo "<p><b>Name:</b> $name</p>";
-}else{
-	echo "<p><b>Name:</b> You do not have a name set</p>";
+if (!isset($_SESSION['username'])) {
+    header('Location: state-demo-1.php');
+    exit;
 }
-echo "<br/><br/>";
-echo "<a href=\"/cgi-bin/php-sessions-1.php\">Session Page 1</a><br/>";
-echo "<a href=\"/php-cgiform.html\">PHP CGI Form</a><br />";
-echo "<form style=\"margin-top:30px\" action=\"/cgi-bin/php-destroy-session.php\" method=\"get\">";
-echo "<button type=\"submit\">Destroy Session</button>";
-echo "</form>";
 
-echo "</body>";
-echo "</html>";
+$username = $_SESSION['username'];
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>State Demo - Page 2</title>
+</head>
+<body>
+	<h1>State Demo - Page 2</h1>
+	<p>Hello, <?php echo htmlspecialchars($username); ?>!</p>
+	<p>This is the second page of the demo. The content from the previous page has been shown via the session.</p>
+	<a href="state-demo-1.php">Session Page 1</a>
+</body>
+</html>
